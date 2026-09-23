77408d257de717c8c2be8bf2081f5e153dad5d1b
ganbozza <ganbozza@gmail.com>
2026-09-23 20:41:08 +0800

    Update controller_node.js

--- web/controller_node.js
+++ web/controller_node.js
@@ -245,9 +245,9 @@ function refreshWidgets(node) {
             const newInputSlotIndex = node.inputs.length-1;
             node.inputs[newInputSlotIndex].widget = {  name : gval.title, _hash_ref : boolWidget._hash_ref };
 
-            const linkId = node.graph._rootGraph.last_link_id++;
-            const newLink = [linkId, 0, 0, node.id, 0, "BOOLEAN"];
-            node.graph._rootGraph.links[linkId] = newLink;
+            const linkId = node.graph.last_link_id++;
+            const newLink = [linkId, 0, 0, -10, 0, "BOOLEAN"];
+            node.graph.links[linkId] = newLink;
             
             /*
             if (saved_conn) {
