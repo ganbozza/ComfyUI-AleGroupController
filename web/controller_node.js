3e3c1e867331ef5af268a5d0a030374af3fc179e
ganbozza <ganbozza@gmail.com>
2026-09-23 09:38:59 +0800

    Fix link property access in controller_node.js

--- web/controller_node.js
+++ web/controller_node.js
@@ -204,7 +204,7 @@ function refreshWidgets(node) {
         }
         if(!node.widgets || !node.widgets.find((w) => w.options.title === gval.title)) {
             const boolWidget = addBooleanWidgetToNode(node, gval.title, gval.value, gval.key);
-            const link_num = prev_inputs.find((p)=>p.widget.name===gval.title)?.link || null;
+            const link_num = prev_inputs.find((p)=>p.widget.name===gval.title)?._link || null;
             node.addInput(gval.title, "BOOLEAN");
             const slot = node.inputs.length-1;
             if(link_num!==null) {
