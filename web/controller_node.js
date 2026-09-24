1521d2727793bfdcf8d10e2fbffcb4846fdf1966
ganbozza <ganbozza@gmail.com>
2026-09-25 00:12:31 +0800

    Update controller_node.js

--- web/controller_node.js
+++ web/controller_node.js
@@ -661,6 +661,7 @@ app.registerExtension({
                 const localWidget = this.widgets[link_info.target_slot];
                 const upstreamWidget = ALEGROUPCONTROLLER_SERVICE.getUpstreamWidgetByLink(link_info, this.graph);
                 syncPromotedWidgetCallback(upstreamWidget, localWidget);
+                this.setDirtyCanvas(true, true);
                   /*
                     if(upstreamWidget && localWidget && localWidget.value!=upstreamWidget.value) {
                        localWidget.value = upstreamWidget.value;
