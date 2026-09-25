751978abf2bacd2f67268fa54a68382380d1f073
ganbozza <ganbozza@gmail.com>
2026-09-25 11:29:06 +0800

    Update controller_node.js

--- web/controller_node.js
+++ web/controller_node.js
@@ -596,27 +596,28 @@ app.registerExtension({
 
         const originalOnNodeCreated = nodeType.prototype.onNodeCreated;
         nodeType.prototype.onNodeCreated = function () {
-          const result = originalOnNodeCreated?.apply(this, arguments);
-            
-        if (!this.properties || typeof this.properties !== "object") {
-            this.properties = {};
-        }
-        if (typeof this.properties[SORT_A_KEY] !== "boolean") {
-            this.properties[SORT_A_KEY] = true;
-        }
-
-        if (typeof this.properties[MATCH_KEY] !== "string") {
-            this.properties[MATCH_KEY] = "";
-        }
-        if (typeof this.properties[ALTERNATE_KEY] !== "string") {
-            this.properties[ALTERNATE_KEY] = "";
-        }
-        if (typeof this.properties[EXCLUDE_KEY] !== "string") {
-            this.properties[EXCLUDE_KEY] = "";
-        }
-        if (typeof this.properties[MUTE_KEY] !== "string") {
-            this.properties[MUTE_KEY] = "";
-        }
+            const result = originalOnNodeCreated?.apply(this, arguments);
+            if(this.is_configured) return result;
+            this.is_configured = true;
+            if (!this.properties || typeof this.properties !== "object") {
+                this.properties = {};
+            }
+            if (typeof this.properties[SORT_A_KEY] !== "boolean") {
+                this.properties[SORT_A_KEY] = true;
+            }
+    
+            if (typeof this.properties[MATCH_KEY] !== "string") {
+                this.properties[MATCH_KEY] = "";
+            }
+            if (typeof this.properties[ALTERNATE_KEY] !== "string") {
+                this.properties[ALTERNATE_KEY] = "";
+            }
+            if (typeof this.properties[EXCLUDE_KEY] !== "string") {
+                this.properties[EXCLUDE_KEY] = "";
+            }
+            if (typeof this.properties[MUTE_KEY] !== "string") {
+                this.properties[MUTE_KEY] = "";
+            }
 
           bindNode(this);
           ALEGROUPCONTROLLER_SERVICE.init();
